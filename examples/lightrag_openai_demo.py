a8e6b23de892179ea41f434fbc090fe7e0fb59cc
Shuojie Wang <latioswang@gmail.com>
2024-12-19 23:59:09 +0800

    Refactor lightrag_openai_demo.py to support dynamic input file handling and integrate Neo4J for graph storage. Updated working directory and added environment variables for OpenAI and Neo4J credentials. Enhanced logging for query execution and file insertion.

--- examples/lightrag_openai_demo.py
+++ examples/lightrag_openai_demo.py
@@ -1,39 +1,62 @@
+import glog as log
+# log.setLevel(log.DEBUG)
 import os
 
 from lightrag import LightRAG, QueryParam
 from lightrag.llm import gpt_4o_mini_complete
 
-WORKING_DIR = "./dickens"
+WORKING_DIR = "./movies"
+INPUT_DIR="./data/"
 
-if not os.path.exists(WORKING_DIR):
-    os.mkdir(WORKING_DIR)
+os.makedirs(WORKING_DIR, exist_ok=True)
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+ 
+# neo4j
+BATCH_SIZE_NODES = 500
+BATCH_SIZE_EDGES = 100
+os.environ["NEO4J_URI"] = "neo4j+s://49341d95.databases.neo4j.io"
+os.environ["NEO4J_USERNAME"] = "neo4j"
+os.environ["NEO4J_PASSWORD"] = "VRNrnd5iPbLlH1RHqoHiRC9DDlHSTE2oDnLCsBF2_bg"
 
 rag = LightRAG(
     working_dir=WORKING_DIR,
     llm_model_func=gpt_4o_mini_complete,
     # llm_model_func=gpt_4o_complete
+    graph_storage="Neo4JStorage",
+    
 )
 
 
-with open("./book.txt", "r", encoding="utf-8") as f:
-    rag.insert(f.read())
+for filename in os.listdir(INPUT_DIR):
+    filepath = os.path.join(INPUT_DIR, filename)
+    if os.path.isfile(filepath):
+        log.info(f"Inserting content from file: {filepath}")
+        with open(filepath, "r", encoding="utf-8") as f:
+            rag.insert(f.read())
 
 # Perform naive search
+query = "What are the top themes?"
+log.info(f"Executing naive search query: {query}")
 print(
-    rag.query("What are the top themes in this story?", param=QueryParam(mode="naive"))
+    rag.query(query, param=QueryParam(mode="naive"))
 )
 
 # Perform local search
+log.info(f"Executing local search query: {query}")
 print(
-    rag.query("What are the top themes in this story?", param=QueryParam(mode="local"))
+    rag.query(query, param=QueryParam(mode="local"))
 )
 
 # Perform global search
+log.info(f"Executing global search query: {query}")
 print(
-    rag.query("What are the top themes in this story?", param=QueryParam(mode="global"))
+    rag.query(query, param=QueryParam(mode="global"))
 )
 
 # Perform hybrid search
+log.info(f"Executing hybrid search query: {query}")
 print(
-    rag.query("What are the top themes in this story?", param=QueryParam(mode="hybrid"))
+    rag.query(query, param=QueryParam(mode="hybrid"))
 )
